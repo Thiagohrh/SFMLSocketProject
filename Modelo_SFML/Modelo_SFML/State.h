b1132b4846b56285ac8b257a71ccc6d837d689ad
Thiago Henrique <thiagohrh@gmail.com>
2019-11-03 02:15:19 -0300

    Added Resources folder. More engine groundworks.

--- Modelo_SFML/Modelo_SFML/State.h
+++ Modelo_SFML/Modelo_SFML/State.h
@@ -7,8 +7,8 @@ public:
 	virtual void Init() = 0;
 
 	virtual void HandleInput() = 0;
-	virtual void Update() = 0;
-	virtual void Draw(float DeltaTime) = 0;
+	virtual void Update(float dt) = 0;
+	virtual void Draw(float dt) = 0;
 	virtual void Pause() {}
 	virtual void Resume() {}
 };
--- Modelo_SFML/Modelo_SFML/main.cpp
+++ Modelo_SFML/Modelo_SFML/main.cpp
@@ -1,27 +1,8 @@
-#include <SFML/Graphics.hpp>
 #include <iostream>
-
-#define SCREEN_WIDTH 1024
-#define SCREEN_HEIGHT 780
+#include "Game.h"
 
 int main() 
 {
-	sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "TikTakToe");
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			switch (event.type)
-			{
-			case sf::Event::Closed:
-				window.close();
-				break;
-			}
-		}
-		window.clear();
-
-		window.display();
-	}
+	Game(800, 600, "SFML Starter");
 	return 0;
 }
